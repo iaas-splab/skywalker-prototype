270a58e2b80651d451e123a65a814c5ad73e4654
kaplanan <ayhankaplan@hotmail.de>
2019-08-27 20:44:31 +0200

    Initialize spring boot backend

---
+++ skywalker/src/main/java/de/iaas/skywalker/SkywalkerApplication.java
@@ -0,0 +1,13 @@
+package de.iaas.skywalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywalkerApplication.class, args);
+	}
+
+}
---
+++ skywalker/src/test/java/de/iaas/skywalker/SkywalkerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.iaas.skywalker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkywalkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
