022af2ba2de5393d218c0f1d8c446d8c6244b380
kaplanan <ayhankaplan@hotmail.de>
2019-08-27 21:19:47 +0200

    Added bootstrap and font-awesome to global styles

--- skywalker/src/main/java/de/iaas/skywalker/SkywalkerApplication.java
+++ skywalker/src/main/java/de/iaas/skywalker/SkywalkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkywalkerApplication {
+public class SkyWalkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkywalkerApplication.class, args);
+		SpringApplication.run(SkyWalkerApplication.class, args);
 	}
 
 }
