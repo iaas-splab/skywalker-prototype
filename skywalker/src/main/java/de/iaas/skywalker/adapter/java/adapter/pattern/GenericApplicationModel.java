7e675ce342e823af24b6596b021277347d3ab416
kaplanan <ayhankaplan@hotmail.de>
2019-09-09 22:37:50 +0200

    Initial approach on simple generation of basic java format adapters

---
+++ skywalker/src/main/java/de/iaas/skywalker/adapter/java/adapter/pattern/GenericApplicationModel.java
@@ -0,0 +1,9 @@
+package de.iaas.skywalker.adapter.java.adapter.pattern;
+
+public interface GenericApplicationModel {
+    String getEvents();
+
+    String getInvokedServices();
+
+    String getFunction();
+}
---
+++ skywalker/src/main/java/de/iaas/skywalker/adapter/java/adapter/pattern/Main.java
@@ -0,0 +1,7 @@
+package de.iaas.skywalker.adapter.java.adapter.pattern;
+
+/**
+ * Created by nymvno on 09.09.2019.
+ */
+public class Main {
+}
