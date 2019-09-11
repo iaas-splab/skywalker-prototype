e51fa73c51779745ec4f68a13fe2162f730f16c8
kaplanan <ayhankaplan@hotmail.de>
2019-09-11 21:56:16 +0200

    Added initialization with working template examples for testing

--- skywalker/src/main/java/de/iaas/skywalker/SkywalkerApplication.java
+++ skywalker/src/main/java/de/iaas/skywalker/SkywalkerApplication.java
@@ -1,14 +1,21 @@
 package de.iaas.skywalker;
 
+import de.iaas.skywalker.models.MappingModule;
+import de.iaas.skywalker.models.Template;
 import de.iaas.skywalker.repository.MappingModuleRepository;
 import de.iaas.skywalker.repository.TemplateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SkywalkerApplication {
@@ -20,6 +27,7 @@ public class SkywalkerApplication {
 	MappingModuleRepository mappingModuleRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SkywalkerApplication.class, args);
 	}
 	@Bean
@@ -32,6 +40,41 @@ public class SkywalkerApplication {
 						.allowedMethods("*");
 			}
 		};
+	}
+
+	@Bean
+	ApplicationRunner init(){
+		return args -> {
+			try {
+				this.initRepos();
+			} catch ( IOException e ) {
+				e.printStackTrace();
+			}
+		};
+	}
+
+	private void initRepos() throws IOException {
+		Template template = new Template();
+		template.setName("serverless.yml");
+		template.setBody(readFileToString(Paths.get("").toAbsolutePath().toString() + "\\src\\main\\resources\\templates\\serverless.yml"));
+		templateRepository.save(template);
+
+		MappingModule mappingModule = new MappingModule();
+		mappingModule.setName("rule_serverless_v2.yaml");
+		mappingModule.setBody(readFileToString(Paths.get("").toAbsolutePath().toString() + "\\src\\main\\resources\\mapping.configurations\\rule_serverless_v2.yaml"));
+		mappingModuleRepository.save(mappingModule);
+	}
+
+	private String readFileToString(String filePath) throws IOException {
+		InputStream is = new FileInputStream(filePath);
+		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+		String line = buf.readLine();
+		StringBuilder sb = new StringBuilder();
+
+		while(line != null){ sb.append(line).append("\n"); line = buf.readLine(); }
+		return sb.toString();
+
 	}
 
 }
