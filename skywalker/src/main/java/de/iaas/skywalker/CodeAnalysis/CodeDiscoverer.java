01530e1c4b69004874b1df96b7d06a07a411ab93
kaplanan <ayhankaplan@hotmail.de>
2019-10-07 18:15:22 +0200

    Method for discovery of relevant import packages with provider specific api usage

--- skywalker/src/main/java/de/iaas/skywalker/CodeAnalysis/CodeDiscoverer.java
+++ skywalker/src/main/java/de/iaas/skywalker/CodeAnalysis/CodeDiscoverer.java
@@ -1,18 +1,40 @@
 package de.iaas.skywalker.CodeAnalysis;
 
+
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.NodeList;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class CodeDiscoverer {
 
     private CompilationUnit compiler;
 
+    private static List<String> sdkLookup = new ArrayList<String>() {{
+        add("amazonaws");
+        add("amazonaws.services");
+        add("lambda");
+    }};
+
     public CodeDiscoverer(String filePath) throws FileNotFoundException {
         this.compiler = StaticJavaParser.parse(new File(filePath));
     }
 
-
+    public void discoverImports() {
+        Set<String> providerLibraries = new HashSet<>();
+        NodeList<ImportDeclaration> imports = this.compiler.getImports();
+        sdkLookup.forEach(term -> {
+            for (ImportDeclaration importDeclaration : imports) {
+                if (importDeclaration.getName().getQualifier().toString().contains(term))
+                    providerLibraries.add(importDeclaration.getName().getIdentifier());
+            }
+        });
+    }
 }
