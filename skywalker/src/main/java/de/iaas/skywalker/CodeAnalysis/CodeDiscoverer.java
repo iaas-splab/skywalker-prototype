538e4937aa4d73d526e48842289fa37a55996f1d
kaplanan <ayhankaplan@hotmail.de>
2019-10-07 19:10:48 +0200

    Implemented subroutine for collecting all relevant field declarations inside a lambda handler class

--- skywalker/src/main/java/de/iaas/skywalker/CodeAnalysis/CodeDiscoverer.java
+++ skywalker/src/main/java/de/iaas/skywalker/CodeAnalysis/CodeDiscoverer.java
@@ -5,6 +5,8 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,10 +14,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CodeDiscoverer {
 
     private CompilationUnit compiler;
+    private List<String> listOfHandlers;
+    private List<String> criticalStmts = new ArrayList<>();
+    private Set<String> criticalTerms = new HashSet<>();
 
     private static List<String> sdkLookup = new ArrayList<String>() {{
         add("amazonaws");
@@ -23,8 +29,9 @@ public class CodeDiscoverer {
         add("lambda");
     }};
 
-    public CodeDiscoverer(String filePath) throws FileNotFoundException {
+    public CodeDiscoverer(String filePath, List<String> listOfHandlers) throws FileNotFoundException {
         this.compiler = StaticJavaParser.parse(new File(filePath));
+        this.listOfHandlers = listOfHandlers;
     }
 
     /**
@@ -44,5 +51,45 @@ public class CodeDiscoverer {
                     providerLibraries.add(importDeclaration.getName().getIdentifier());
             }
         });
+    }
+
+    private boolean isLambdaHandler(ClassOrInterfaceDeclaration javaClass) {
+        boolean implementsInterface =
+                (!javaClass.getImplementedTypes().stream()
+                        .filter(x -> x.getName().toString().equals("RequestHandler"))
+                        .collect(Collectors.toList())
+                        .isEmpty());
+        boolean isHandler = this.listOfHandlers.contains(javaClass.getNameAsString());
+        return isHandler && implementsInterface;
+    }
+
+    private void collectCriticalFieldDeclarations(ClassOrInterfaceDeclaration javaClass) {
+        List<FieldDeclaration> fieldDeclarations = (List<FieldDeclaration>)
+                javaClass.getMembers().stream().filter(member -> member instanceof FieldDeclaration);
+
+        for (FieldDeclaration field : fieldDeclarations) {
+            this.criticalTerms.forEach(type -> {
+                if (field.toString().contains(type)) {
+                    this.criticalStmts.add(field.toString());
+                    this.criticalTerms.add(field.getVariable(0).getNameAsString());
+                }
+            });
+        }
+    }
+
+    public int discoverClassMethods() {
+        ClassOrInterfaceDeclaration javaClass =
+                (ClassOrInterfaceDeclaration) this.compiler.getChildNodes().stream()
+                        .filter(x -> x instanceof ClassOrInterfaceDeclaration).collect(Collectors.toList()).get(0);
+
+        if (!isLambdaHandler(javaClass)) return 0;
+
+        collectCriticalFieldDeclarations(javaClass);
+        // find all methods and go through them
+        // // find all local fields in the method and add to criticalTerms + criticalStmts
+        // // // iterate all remaining stmts (ForEach, If, etc.) and add them to criticalStmts, if suitable
+
+
+        return 0;
     }
 }
